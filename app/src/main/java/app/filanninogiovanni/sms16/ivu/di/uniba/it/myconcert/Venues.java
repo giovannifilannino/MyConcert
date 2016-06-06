3237e66cefc9c28e9e1afab5a9a0941ce900687a
redjin <gianni.filannino@gmail.com>
2016-06-06 12:20:24 +0200

    implementato secondo parser per database setlist, restituisce nome sede quando inserito

---
+++ app/src/main/java/app/filanninogiovanni/sms16/ivu/di/uniba/it/myconcert/Venues.java
@@ -0,0 +1,25 @@
+package app.filanninogiovanni.sms16.ivu.di.uniba.it.myconcert;
+
+/**
+ * Created by Giovanni on 06/06/2016.
+ */
+public class Venues {
+    String id;
+    String name;
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
