a3d8d52ab3cb13d6843d092b9bcff3dd5b652892
redjin <gianni.filannino@gmail.com>
2016-06-14 18:38:08 +0200

    implementata ricerca combinata

--- app/src/main/java/app/filanninogiovanni/sms16/ivu/di/uniba/it/myconcert/search_fragment.java
+++ app/src/main/java/app/filanninogiovanni/sms16/ivu/di/uniba/it/myconcert/search_fragment.java
@@ -27,12 +27,14 @@ public class search_fragment extends Fragment {
     private XMLSetListParser setListParser;
     private  String URL_ARTIST = "http://api.setlist.fm/rest/0.1/search/setlists?artistName=";
     private String URL_VENUES = "http://api.setlist.fm/rest/0.1/search/setlists?venueName=";
+    private String URL_COMBINED = "&venueName=";
     private EditText name_artist;
     private EditText name_venue;
     private Button search_button;
-    private boolean flag=true;
     private OnSearch onSearch;
     private LoadSetListXMLData loadSetListXMLData = new LoadSetListXMLData();
+
+
 
     @Override
     public void onAttach(Context context) {
@@ -69,47 +71,32 @@ public class search_fragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        loadSetListXMLData=null;
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        loadSetListXMLData=null;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        loadSetListXMLData=new LoadSetListXMLData();
-    }
-
     View.OnClickListener artistSearchButton = new View.OnClickListener() {
         @Override
-            public void onClick(View v) {
-            if(name_artist.getText().toString().compareTo("") != 0){
+        public void onClick(View v) {
+            if(name_artist.getText().toString().compareTo("") != 0 && name_venue.getText().toString().compareTo("") != 0){
+                String artist = name_artist.getText().toString();
+                String venues = name_venue.getText().toString();
+                artist = artist.replaceAll("\\s+","%20");
+                venues = venues.replaceAll("\\s+","%20");
+                query = URL_ARTIST + '"' + artist + '"' + URL_COMBINED + '"' + venues + '"';
+                loadSetListXMLData.execute(query);
+            }
+            else if(name_artist.getText().toString().compareTo("") != 0){
                 String artist = name_artist.getText().toString();
                 artist = artist.replaceAll("\\s+","%20");
                 query = URL_ARTIST + '"' + artist + '"';
                 loadSetListXMLData.execute(query);
-                flag=false;
             } else if(name_venue.getText().toString().compareTo("") != 0){
                 String venues = name_venue.getText().toString();
                 venues = venues.replaceAll("\\s+","%20");
                 query = URL_VENUES + '"' + venues + '"';
                 loadSetListXMLData.execute(query);
-                flag=true;
             }
 
 
         }
     };
-
-
-
 
 
 
@@ -126,20 +113,7 @@ public class search_fragment extends Fragment {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             ArrayList<Setlist> setList = setListParser.parderData;
-            if(setList.size()!=0){
-                onSearch.searchStart(setList);
-            }
-            else {
-                if(flag==true){
-                    ErrorClass.onCreateDialog(ErrorClass.DIALOG_NOVENUESFOUND_ID,getActivity());
-                    onDestroy();
-                    onResume();
-                }
-                else {
-                    ErrorClass.onCreateDialog(ErrorClass.DIALOG_NOARTISTFOUND_ID,getActivity());
-                    onDestroy();onResume();
-                }
-            }
+            onSearch.searchStart(setList);
         }
     }
 
