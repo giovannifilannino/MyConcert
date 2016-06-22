79467ba02b6ff4366b0879e15299281a5aa5892d
Draven94 <dinoia.miguel@gmail.com>
2016-06-22 17:58:31 +0200

    r

--- app/src/main/java/app/filanninogiovanni/sms16/ivu/di/uniba/it/myconcert/Artista/ArtistaHome.java
+++ app/src/main/java/app/filanninogiovanni/sms16/ivu/di/uniba/it/myconcert/Artista/ArtistaHome.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -40,7 +42,7 @@ public class ArtistaHome extends Fragment {
     private ListView listaCanzoni;
     private ImageView artistImage;
 
-
+    private Toolbar toolbar;
     private String urlImmagine;
     private String nomeArtistaString;
     private String cognomeArtistaString;
@@ -58,17 +60,12 @@ public class ArtistaHome extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        nomeArtista =(TextView) getActivity().findViewById(R.id.nomeArtistaID);
-        cognomeArtista = (TextView) getActivity().findViewById(R.id.cognomeArtistaID);
-        aliasArtista = (TextView) getActivity().findViewById(R.id.aliasArtist);
         listaCanzoni = (ListView) getActivity().findViewById(R.id.listTopFiveSongs);
         artistImage = (ImageView) getActivity().findViewById(R.id.artista_immagine);
         requestQueue = Volley.newRequestQueue(getActivity());
-
-        nomeArtista.setText(nomeArtistaString);
-        cognomeArtista.setText(cognomeArtistaString);
-        aliasArtista.setText(aliasArtistaString);
-
+        toolbar=(Toolbar)getActivity().findViewById(R.id.tool_bar_artista);
+        toolbar.setTitle(aliasArtistaString);
+        toolbar.setSubtitle(nomeArtistaString+" "+cognomeArtistaString);
 
         ImageRequest imageRequest = new ImageRequest(urlImmagine, new Response.Listener<Bitmap>() {
             @Override
@@ -93,7 +90,7 @@ public class ArtistaHome extends Fragment {
 
         fillSongArray(url);
 
-        listaCanzoni.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,songArray));
+
     }
 
     private void fillSongArray(String url){
@@ -105,8 +102,8 @@ public class ArtistaHome extends Fragment {
                     for(int i=0; i<5; i++){
                         jsonObject = response.getJSONObject(i);
                         songArray.add(jsonObject.getString("NomeCanzone"));
-
                     }
+                    listaCanzoni.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,songArray));
 
                 }catch (Exception e){
                     e.printStackTrace();
