c2c3c0a42883f133e220a47ead5af4f28d7a824c
mikidelve <mikidelve@gmail.com>
2016-07-09 12:12:33 +0200

    Playlist

--- app/src/main/java/app/filanninogiovanni/sms16/ivu/di/uniba/it/myconcert/Playlist_fragment.java
+++ app/src/main/java/app/filanninogiovanni/sms16/ivu/di/uniba/it/myconcert/Playlist_fragment.java
@@ -1,37 +1,27 @@
 package app.filanninogiovanni.sms16.ivu.di.uniba.it.myconcert;
 
 import android.app.Fragment;
-import android.content.Context;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.util.Pair;
+
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-import app.filanninogiovanni.sms16.ivu.di.uniba.it.myconcert.Adapter.AdapterCardTweet;
 import app.filanninogiovanni.sms16.ivu.di.uniba.it.myconcert.Adapter.PlaylistAdapter;
 import app.filanninogiovanni.sms16.ivu.di.uniba.it.myconcert.Entities.Setlist;
 
